d2092b77a85198eac1b56b063c57f0197397f85c
Abhay Kumar <abhay0480sharma@gmail.com>
2023-07-22 19:02:46 +0530

    icon attribute change

--- app/Components/Button.js
+++ app/Components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Button({btnText,styles,links}) {
   return (
     <div>
-       <a href={links} className={`${styles} px-7 py-3 rounded-lg my-6 flex`} >{btnText=="Email"?<img src="/Mail.png" alt="" className='mr-1 object-cover'/>:<img src="/linkedin.png" alt="" className='mr-1 object-cover '/>} {btnText}</a>
+       <a href={links} className={`${styles} px-7 py-3 rounded-lg my-6 flex`} >{btnText=="Email"?<img src="/Mail.png" alt="" className='mr-1 object-contain'/>:<img src="/linkedin.png" alt="" className='mr-1 object-contain '/>} {btnText}</a>
     </div>
   )
 }
